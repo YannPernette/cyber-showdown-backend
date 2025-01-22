1101770e333475237b0b01dc60df8594f8c9d218
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2025-01-22 10:02:46 +0100

    feat(data-auth): définition de la table users dans la base de données

---
+++ src/config/database.js
@@ -0,0 +1,31 @@
+// import de sqlite3
+const sqlite3 = require("sqlite3").verbose(); // verbose pour obtenir plus d'informations
+
+const db = new sqlite3.Database("./cyber-showdown.db", (err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données : ", err);
+  } else {
+    console.log("Connecté à la base de données");
+    initDatabase();
+  }
+});
+
+function initDatabase() {
+  db.serialize(() => {
+    // table avec les infos des utilisateurs
+    db.run(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        username TEXT,
+        password TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        description TEXT,
+        profile_picture BLOB
+      );
+    `);
+  });
+}
+
+// export de la base de données pour pouvoir l'importer dans app.js
+module.exports = db;
